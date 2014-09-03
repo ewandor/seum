834eece35a15656f355c89c9bf929effcdae707c
ewandor <ewandor@dorfsvald.net>
2014-09-03 17:39:23 +0200

    starting the regex

--- se-manager/__init__.py
+++ se-manager/__init__.py
@@ -2,16 +2,18 @@ __author__ = 'ggentile'
 
 import re
 
+
+class LogLine:
+    LINE_REGEX = r'(?P<date_time>\d{4}-\d{2}-\{2} \d{2}:\d{2}:\d{2}.\d{3}) - Thread:   \d+ -> +(?P:<body>\w.*)'
+
+    def __init__(self, line):
+        result = re.search(self.LINE_REGEX, line)
+        self.date = result.match('date_time')
+        self.body = result.match('body')
+
 test_file = '/home/ggentile/PycharmProjects/se-manager/SpaceEngineersDedicated_20140902_185151.log'
 
 log_file = open(test_file, 'r')
 
 for line in log_file:
-    print(line)
-
-
-class LogLine:
-    LINE_REGEX = r''
-
-    def __init__(self, line):
-        pass
+    logLine = LogLine(line)
