f7f0766ae68629cbbfb7ea169dd08ad2ad020d8f
ewandor <ewandor@dorfsvald.net>
2014-09-05 14:11:55 +0200

    Implementing user_id following

--- se-manager/ServiceManager.py
+++ se-manager/ServiceManager.py
@@ -1,3 +1,4 @@
+from multiprocessing import connection
 import os
 import re
 import subprocess
@@ -5,10 +6,15 @@ from pip.vendor.distlib._backport import shutil
 
 __author__ = 'ggentile'
 
+class User:
+    def __init__(self, login, date_connection ):
+        self.login = login
+        self.date_connection = date_connection
 
 class LogsManager:
-    REGEX_USER_CONNECTED = r'World request received: (?P<login>)'
-    REGEX_USER_DISCONECTED = r'User left (?P<login>)'
+    REGEX_USER_CONNECTED = r'World request received: (?P<login>.+)'
+    REGEX_USER_ID = r'Server ValidateAuthTicketResponse (k_EAuthSessionResponseOK), owner: (?P<user_id>\d+)'
+    REGEX_USER_DISCONNECTED = r'User left (?P<login>.+)'
 
     def purge_logs(self):
         for files in logs_dir:
@@ -16,14 +22,23 @@ class LogsManager:
 
     def list_users(self):
         connected_users = []
+        next_one_is_user_id = False
         for logLine in self.current_log_file:
-            result = re.search(self.REGEX_USER_CONNECTED, logLine.body)
-            if result:
-                connected_users.append(result.group('login'))
+            if next_one_is_user_id:
+                result = re.search(self.REGEX_USER_ID, logLine.body)
+                connected_users[-1].user_id = result.group('user_id')
             else:
-                result = re.search(self.REGEX_USER_DISCONECTED, logLine.body)
-                if result and result.group('login') in connected_users:
-                    connected_users.remove(result.group('login'))
+                result = re.search(self.REGEX_USER_CONNECTED, logLine.body)
+                if result:
+                    connected_users.append(User(result.group('login'), logLine.date))
+                    next_one_is_user_id = True
+                else:
+                    result = re.search(self.REGEX_USER_DISCONECTED, logLine.body)
+                    if result and result.group('login') in connected_users:
+                        for user in connected_users:
+                            if user.login == result.group('login'):
+                                connected_users.remove(user)
+                                break
 
         return connected_users
 
