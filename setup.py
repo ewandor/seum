237d5d0671b876d241a001704c1fb9e9a0c8c60e
G Gentile <ggentile@x2i.fr>
2016-01-19 11:35:01 +0100

    Changing directory structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='seum',
+      version='0.1',
+      description='Space Engineer Utility Manager',
+      url='http://github.com/ewandor/seum',
+      author='Ewandor',
+      author_email='ewandor@dorfsvald.net',
+      license='MIT',
+      packages=['seum'],
+      zip_safe=False)
